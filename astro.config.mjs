fef893d1d9d89219748ab3bf76179f6a8a3c3185
Jesus Franco Martinez <jesus.franco@ventionteams.com>
2024-09-15 04:42:53 -0600

    waiting for db

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,14 @@ import analogjsangular from '@analogjs/astro-angular';
 import node from '@astrojs/node';
 import vercel from '@astrojs/vercel/serverless';
 
-import db from '@astrojs/db';
+//import db from '@astrojs/db';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [analogjsangular(), db()],
+  integrations: [
+    analogjsangular(),
+    //db()
+  ],
   output: 'server',
   prefetch: true,
 
